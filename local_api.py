2a9e7153dfe2330e91e23495e22f9d5c3f82ad55
mkit359 <meganlynn3493@aol.com>
2025-07-21 21:34:35 -0400

    Update local_api.py

--- local_api.py
+++ local_api.py
@@ -2,13 +2,12 @@ import json
 
 import requests
 
-# TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+URL = 'http://127.0.0.1:8000'
+r = requests.get(URL)
 
-# TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+print('Status Code:', r.status_code)
+
+print('Response:', r.json())
 
 
 
@@ -29,10 +28,8 @@ data = {
     "native-country": "United-States",
 }
 
-# TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post('http://127.0.0.1:8000/data/', json=data)
 
-# TODO: print the status code
-# print()
-# TODO: print the result
-# print()
+print('Status Code:', r.status_code)
+
+print('Response:', r.json())
